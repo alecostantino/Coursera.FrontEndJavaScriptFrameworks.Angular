f04ff2404bc1acaa38e5108b8ef27d620a9d7ec5
acostantino <acostantino@BFNB13583>
2018-04-23 17:31:50 -0300

    Reactive Forms Part 2

--- Angular/conFusion/src/app/contact/contact.component.ts
+++ Angular/conFusion/src/app/contact/contact.component.ts
@@ -20,12 +20,30 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  //createForm() {
+  //  this.feedbackForm = this.fb.group({
+  //    firstname: '',
+  //    lastname: '',
+  //    telnum: 0,
+  //    email: '',
+  //    agree: false,
+  //    contacttype: 'None',
+  //    message: ''
+  //  });
+  //}
+
+  //onSubmit() {
+  //  this.feedback = this.feedbackForm.value;
+  //  console.log(this.feedback);
+  //  this.feedbackForm.reset();
+  //}
+
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: ['', Validators.required],
+      email: ['', Validators.required],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -35,7 +53,15 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
   }
 
 }
