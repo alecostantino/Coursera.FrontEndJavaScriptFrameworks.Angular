71534c221dc34a3da648ce94c46f67e70ce40f4e
acostantino <acostantino@BFNB13583>
2018-04-27 12:07:19 -0300

    Animations Part 1

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -7,11 +7,25 @@ import 'rxjs/add/operator/switchMap';
 //Assignment 3 - Task 2
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -32,7 +46,8 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
   errMess: string;
   dishcopy = null;
-  
+  visibility = 'shown';
+
   formErrors = {
     'rating': '',
     'comment': '',
@@ -77,12 +92,28 @@ export class DishdetailComponent implements OnInit {
     //    this.dishservice.getDish(+params['id'])).subscribe(
     //  dish => { this.dish = dish; this.setPrevNext(dish.id); },
     //  errMess => this.errMess = errMess);
+    //this.route.params
+    //  .switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
+    //  .subscribe(dish => {
+    //    this.dish = dish;
+    //    this.dishcopy = dish;
+    //    this.setPrevNext(dish.id);
+    //  },
+    //  errmess => {
+    //    this.dish = null;
+    //    this.errMess = <any>errmess;
+    //  });
+
     this.route.params
-      .switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
+      .switchMap((params: Params) => {
+        this.visibility = 'hidden';
+        return this.dishservice.getDish(+params['id']);
+      })
       .subscribe(dish => {
         this.dish = dish;
         this.dishcopy = dish;
         this.setPrevNext(dish.id);
+        this.visibility = 'shown';
       },
       errmess => {
         this.dish = null;
