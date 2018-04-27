a84e4b0fb6fd232f4001591d0841bfaf8b896d5f
acostantino <acostantino@BFNB13583>
2018-04-27 17:27:14 -0300

    Component Test

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { Material } from '@angular/material';
+//import { MaterialModule } from '@angular/material';
 import {
   MatButtonModule,
   MatCheckboxModule,
